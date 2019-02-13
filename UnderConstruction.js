f8c4530060f4aca367d2f7fc4a00f6e9237058f4
acm <acm@dynamicwebapac.com>
2019-02-13 17:11:16 +0630

    code

--- UnderConstruction.js
+++ UnderConstruction.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+
 	if( document.getElementById('UnderConstruction')){
+		var UCEle = document.getElementById("UnderConstruction");
+		var UCText = UCEle.innerText;
+
 		var infoDiv = document.createElement("DIV");
 		infoDiv.classList.add('info');
 
+		var img = document.createElement("IMG");
+		img.classList.add('infoSignImg');
+		img.setAttribute("src", "/images/circle.png");
 		
+
 	}
 }, false);
