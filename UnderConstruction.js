9407fcaa110611cdc99b75a80c00d6ea533827ac
acm <acm@dynamicwebapac.com>
2019-02-14 17:03:23 +0630

    new 14Feb

--- UnderConstruction.js
+++ UnderConstruction.js
@@ -1,18 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(){
-
-	console.log("UnderConstruction On");
+﻿document.addEventListener('DOMContentLoaded', function(){
 
 	if( document.getElementById('UnderConstruction')){
 		var UCEle = document.getElementById("UnderConstruction");
-		var UCText = UCEle.innerText;
+		var UCText = "In Maintainence" // Must be dynamic later
 
 		var infoDiv = document.createElement("DIV");
+      	infoDiv.setAttribute('id', 'UCBody' );
 		infoDiv.classList.add('info');
 
-		var img = document.createElement("IMG");
-		img.classList.add('infoSignImg');
-		img.setAttribute("src", "/images/circle.png");
+		var infoSign = document.createElement("SPAN");
+		infoSign.setAttribute('class', 'infoSign');
+		infoSign.innerText = '!';
+      
+      	var infoText1 = document.createElement("SPAN");
+      	infoText1.setAttribute('class', 'infoText1');
+      	infoText1.innerText = UCText;
 		
-
+      	infoDiv.append(infoSign);
+      	infoDiv.append(infoText1);
+      
+      	document.getElementsByTagName('body')[0].append(infoDiv);
 	}
 }, false);
